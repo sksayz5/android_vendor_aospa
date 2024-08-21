d45eaea53deff3bbde5a9dc9eb46b0736f6a3264
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2024-08-21 12:38:42 +0530

    Update AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -53,6 +53,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/phone2/aospa_phone2.mk \
     $(LOCAL_DIR)/porsche/aospa_porsche.mk \
     $(LOCAL_DIR)/psyche/aospa_psyche.mk \
+    $(LOCAL_DIR)/psyche/aospa_RMX1901.mk \
     $(LOCAL_DIR)/raven/aospa_raven.mk \
     $(LOCAL_DIR)/sky/aospa_sky.mk \
     $(LOCAL_DIR)/spes/aospa_spes.mk \
@@ -101,6 +102,7 @@ COMMON_LUNCH_CHOICES += \
     aospa_phone2-userdebug \
     aospa_porsche-userdebug \
     aospa_psyche-userdebug \
+    aospa_RMX1901-userdebug \
     aospa_raven-userdebug \
     aospa_sky-userdebug \
     aospa_spes-userdebug \
